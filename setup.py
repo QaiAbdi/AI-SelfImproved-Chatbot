fe8cdfff5f9d0d6e1f073c67f60af0e749fba92c
Farhad Abdi <qai.abdi@gmail.com>
2021-06-25 07:19:14 +0430

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, version
+
+setup(
+    name = 'XChat',
+    version = '0.0.1',
+    description = 'Self Learning Based ChatBot',
+    author = 'Farhad Abdi',
+    author_mail = 'qai.abdi@gmail.com',
+    packages = [''],
+    install_requires = ['']
+)
